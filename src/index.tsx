2904f7cc156bd74082fe16ee74d1b3f492afe65b
geleeroyale <krassvs@gmail.com>
2026-01-14 11:26:11 +0100

    add normalized path redirect

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Placeholder } from "./screens/Placeholder";
 
+const normalizedPath = window.location.pathname.replace(/\/+$/, "");
+if (normalizedPath === "/secret") {
+  window.location.replace("https://thedao-frontend.blossom.deno.net/");
+}
+
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
     <Placeholder />
-  </StrictMode>,
+  </StrictMode>
 );
